1cf1c26512820be761e7f962292c21ddf7c35140
Tanya Gupta <tanya.harsh@gmail.com>
2017-08-31 11:55:09 +0530

    First Commit

--- lab5.java
+++ lab5.java
@@ -36,10 +36,110 @@ class Reader
     }
 }
 
+class TreeNode<T extends Comparable<T>> implements Comparable<TreeNode<T>>{
+    private T engraving;
+    private TreeNode<T> left;
+    private TreeNode<T> right;
+
+    public TreeNode(T engraving){
+        this.engraving = engraving;
+    }
+
+    public T getEngraving(){
+        return engraving;
+    }
+
+    public TreeNode<T> getRight(){
+        return right;
+    }
+
+    public TreeNode<T> getLeft(){
+        return left;
+    }
+
+    public void setRight(TreeNode<T> t){
+        right = t;
+    }
+
+    public void setLeft(TreeNode<T> t){
+        left = t;
+    }
+
+    @Override
+    public int compareTo(TreeNode<T> t){
+        return engraving.compareTo(t.getEngraving());
+    }
+}
+
+class Tree<T extends Comparable<T>>{
+    private TreeNode<T> root;
+
+    public Tree(){
+        this.root = null;
+    }
+
+    public TreeNode<T> getRoot(){
+        return root;
+    }
+
+    public void insert(T engraving){
+        TreeNode<T> newNode = new TreeNode<T>(engraving);
+
+        if(root==null){
+            root = newNode;
+        }
+        else{
+            TreeNode<T> currentNode = root;
+            TreeNode<T> parent;
+            while(true){
+                parent = currentNode;
+                if(newNode.compareTo(parent)<0){
+                    currentNode = currentNode.getLeft();
+                    if(currentNode==null){
+                        parent.setLeft(newNode);
+                        return;
+                    }
+                }
+
+                if(newNode.compareTo(parent)>0){
+                    currentNode = currentNode.getRight();
+                    if(currentNode==null){
+                        parent.setRight(newNode);
+                        return;
+                    }
+
+                }
+
+            }
+        }
+    }
+
+    public void inOrder(TreeNode<T> t){
+        if(t==null){
+            return;
+        }
+        inOrder(t.getLeft());
+        System.out.println(t.getEngraving()+" ");
+        inOrder(t.getRight());
+
+    }
+}
+
 class App{
     public static void main(String[] args) throws IOException {
 
         Reader rd = new Reader();
+
+        int numTrees = rd.nextInt();
+        int numStudents = rd.nextInt();
+
+        
+
+
+
+        
+        
+
         
     }
 }
